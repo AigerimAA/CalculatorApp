f8c1d2f0d649a3d49688d77359f294285561ac22
Aigerim <aigerim.10803@gmail.com>
2025-12-10 10:24:01 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CalculatorApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Calculator App!");
+            Console.WriteLine("Insert the first number: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insert the second number: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Result of addition: {a + b}");
+        }
+    }
+}
